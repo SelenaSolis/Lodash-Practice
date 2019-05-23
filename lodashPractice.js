a989f6bddd1a15688124dbd947f3a8def9e38a1a
Selena Solis <lenansolis@gmail.com>
2019-05-22 20:43:12 -0500

    comments

--- lodashPractice.js
+++ lodashPractice.js
@@ -3,10 +3,20 @@ var _ = require('lodash');
 
 let a = [1,2,3,4,5,6,7]
 
+console.log("Original Array: ");
+console.log(a);
+
+console.log("Chunk into 2: ");
 console.log(_.chunk(a, 2));
 
-console.log(_.reverse([1,2,3,4,5,6,7]));
+console.log("Reverse: ");
+console.log(_.reverse(a));
 
-console.log(_.without([1,2,3,4,5,6], 1,2,3));
+console.log("Without 1, 2, and 3: "); 
+console.log(_.without(a, 1,2,3));
 
-console.log(_.shuffle([1,2,3,4,5,6,7,8,9]))
+console.log("Shuffled: "); 
+console.log(_.shuffle(a));
+
+console.log("Index of 4: ");
+console.log(_.indexOf(a, 4));
